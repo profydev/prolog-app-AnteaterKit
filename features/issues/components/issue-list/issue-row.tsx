7c0ec2fe0992c8eb9c708483f7f5b86c56367de2
Aleksandr Shatilov <ashatilovdev@gmail.com>
2022-11-30 22:59:03 +0300

    fix(fix-issue-list-number-of-events-and-users) users

--- features/issues/components/issue-list/issue-row.tsx
+++ features/issues/components/issue-list/issue-row.tsx
@@ -48,7 +48,7 @@ const ErrorType = styled.span`
 `;
 
 export function IssueRow({ projectLanguage, issue }: IssueRowProps) {
-  const { name, message, stack, level, numEvents } = issue;
+  const { name, message, stack, level, numEvents, numUsers } = issue;
   const firstLineOfStackTrace = stack.split("\n")[1];
 
   return (
@@ -72,7 +72,7 @@ export function IssueRow({ projectLanguage, issue }: IssueRowProps) {
         </Badge>
       </Cell>
       <Cell>{numEvents}</Cell>
-      <Cell>{numEvents}</Cell>
+      <Cell>{numUsers}</Cell>
     </Row>
   );
 }
