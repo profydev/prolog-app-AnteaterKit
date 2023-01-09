7073f56c8dad9dcae790898487e195bc626330d8
Aleksandr Shatilov <ashatilovdev@gmail.com>
2023-01-09 11:35:22 +0300

    ui button

---
+++ features/ui/button-ui/button-ui.tsx
@@ -0,0 +1,201 @@
+import styled, { css } from "styled-components";
+import { breakpoint, color } from "@styles/theme";
+
+export enum ButtonSize {
+  sm = "sm",
+  md = "md",
+  lg = "lg",
+  xl = "xl",
+}
+
+export enum ButtonColor {
+  primary = "primary",
+  secondary = "secondary",
+  empty = "empty",
+  gray = "gray",
+  emptyGray = "empty gray",
+  error = "error",
+}
+
+export enum IconPosition {
+  leading = "leading",
+  trailing = "trailing",
+  only = "",
+}
+
+export interface UIButtonProps {
+  children: React.ReactNode;
+  color?: ButtonColor;
+  size?: ButtonSize;
+  icon?: React.ReactNode;
+}
+
+const ButtonRoot = styled.button<{ size: ButtonSize }>`
+  background: ${color("primary", 600)};
+  color: #ffffff;
+  width: fit-content;
+  padding: 8px 14px 8px 14px;
+  border: 1px solid #7f56d9;
+  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: 500;
+
+  ${(props) => {
+    switch (props.size) {
+      case ButtonSize.md:
+        return css`
+          padding: 10px 16px 10px 16px;
+        `;
+      case ButtonSize.lg:
+        return css`
+          padding: 10px 18px 10px 18px;
+        `;
+      case ButtonSize.xl:
+        return css`
+          padding: 12px 20px 12px 20px;
+        `;
+    }
+  }};
+
+  ${(props) => {
+    switch (props.color) {
+      case ButtonColor.primary:
+        return css`
+          background: ${color("primary", 600)};
+          color: #ffffff;
+        `;
+      case ButtonColor.secondary:
+        return css`
+          background: ${color("primary", 50)};
+          border: none;
+          color: ${color("primary", 700)};
+        `;
+      case ButtonColor.empty:
+        return css`
+          background: none;
+          border: none;
+          color: ${color("primary", 700)};
+        `;
+      case ButtonColor.gray:
+        return css`
+          background: none;
+          border: 1px solid ${color("gray", 300)};
+          color: ${color("gray", 700)};
+        `;
+      case ButtonColor.emptyGray:
+        return css`
+          background: none;
+          border: none;
+          color: ${color("gray", 500)};
+        `;
+      case ButtonColor.error:
+        return css`
+          background: ${color("error", 600)};
+          border: none;
+          color: #ffffff;
+        `;
+    }
+
+    switch (props.size) {
+      case ButtonSize.md:
+        return css`
+          padding: 10px 16px 10px 16px;
+        `;
+      case ButtonSize.lg:
+        return css`
+          padding: 10px 18px 10px 18px;
+        `;
+      case ButtonSize.xl:
+        return css`
+          padding: 12px 20px 12px 20px;
+        `;
+    }
+  }};
+
+  &:hover {
+    cursor: pointer;
+    ${(props) => {
+      switch (props.color) {
+        case ButtonColor.primary:
+          return css`
+            background: ${color("primary", 700)};
+          `;
+        case ButtonColor.secondary:
+          return css`
+            background: ${color("primary", 100)};
+          `;
+        case ButtonColor.empty:
+          return css`
+            background: ${color("primary", 50)};
+            border: none;
+            color: ${color("primary", 700)};
+          `;
+        case ButtonColor.gray:
+          return css`
+            background: ${color("gray", 300)};
+            color: ${color("gray", 800)};
+          `;
+        case ButtonColor.emptyGray:
+          return css`
+            background: ${color("gray", 50)};
+            color: ${color("gray", 500)};
+          `;
+        case ButtonColor.error:
+          return css`
+            background: ${color("error", 700)};
+          `;
+      }
+    }};
+  }
+
+  &:focus {
+    cursor: pointer;
+    ${(props) => {
+      switch (props.color) {
+        case ButtonColor.primary:
+          return css`
+            background: blue;
+            box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05),
+              0px 0px 0px 4px #f4ebff;
+          `;
+        case ButtonColor.secondary:
+          return css`
+            background: ${color("primary", 100)};
+          `;
+        case ButtonColor.empty:
+          return css`
+            background: ${color("primary", 50)};
+            border: none;
+            color: ${color("primary", 700)};
+          `;
+        case ButtonColor.gray:
+          return css`
+            background: ${color("gray", 300)};
+            color: ${color("gray", 800)};
+          `;
+        case ButtonColor.emptyGray:
+          return css`
+            background: ${color("gray", 50)};
+            color: ${color("gray", 500)};
+          `;
+        case ButtonColor.error:
+          return css`
+            background: ${color("error", 700)};
+          `;
+      }
+    }};
+  }
+`;
+
+export function UIButton({
+  children,
+  size = ButtonSize.md,
+  color,
+}: UIButtonProps) {
+  return (
+    <ButtonRoot size={size} color={color}>
+      {children}
+    </ButtonRoot>
+  );
+}
---
+++ features/ui/button-ui/button.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { UIButton } from ".//button-ui";
+import { size } from "lodash";
+
+export default {
+  title: "UI/Button",
+  component: UIButton,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof UIButton>;
+
+const Template: ComponentStory<typeof UIButton> = ({ size, color }) => (
+  <div style={{ padding: 50 }}>
+    <UIButton size={size} color={color}>
+      Button
+    </UIButton>
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
+Default.parameters = {
+  viewMode: "docs",
+};
